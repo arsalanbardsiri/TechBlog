887db3d64d8190b6ee3b533da8f703f31b6d71eb
AB <35473718+arsalanbardsiri@users.noreply.github.com>
2023-12-22 20:26:43 -0800

    Portions of this project were based on responses provided by [Original Author/Source]. This material has been used with permission for educational purposes in compliance with the edX Boot Camps honor code.

---
+++ README.md
@@ -0,0 +1,3 @@
+# 14 Model-View-Controller (MVC): Tech Blog
+
+Portions of this project were based on responses provided by [Original Author/Source]. This material has been used with permission for educational purposes in compliance with the edX Boot Camps honor code.
---
+++ server.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const helpers = require('./utils/helpers');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sequelize = require('./config/config');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: process.env.SESSION_SECRET,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+    checkExpirationInterval: 15 * 60 * 1000, // The interval at which to cleanup expired sessions (in milliseconds).
+    expiration: 24 * 60 * 60 * 1000  // The maximum age (in milliseconds) of a valid session.
+  })
+};
+
+app.use(session(sess));
+
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(require('./controllers/'));
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
+  sequelize.sync({ force: false });
+});
